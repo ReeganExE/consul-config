c65445124a7c356e9e73b7e811c334cf9668d4f3
ReeganExE <dongian.rapclubkhtn@gmail.com>
2018-11-07 22:36:34 +0700

    Remove unused

--- start_consul.sh
+++ start_consul.sh
@@ -4,10 +4,10 @@ set -e
 
 run_consul()
 {
-	exec consul agent -config-file=/opt/rancher/config/server.json -data-dir=/var/consul
+	exec consul agent -config-file=/opt/rancher/config/config.json -data-dir=/var/consul
 }
 
-while [ ! -f "/opt/rancher/config/server.json" ]; do
+while [ ! -f "/opt/rancher/config/config.json" ]; do
   echo "Configuration file is not ready yet.. sleeping"
 	sleep 1
 done
